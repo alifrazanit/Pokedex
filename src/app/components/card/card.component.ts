3bf34b743b634183cc6743b7fd6913100c51a1e1
Alif Razan Saputra <alifrazanit@gmail.com>
2025-03-06 00:19:03 +0700

    card design pokedex

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
