4832b61c47936c8a22ce49d838942b21a7718d40
Alif Razan Saputra <alifrazanit@gmail.com>
2025-03-05 15:50:16 +0700

    update design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { BannerComponent } from '@components/banner/banner.component';
+import { CardComponent } from '@components/card/card.component';
 import { FieldSearchComponent } from '@components/field-search/field-search.component';
 import { HeaderComponent } from '@components/header/header.component';
 
@@ -10,7 +11,8 @@ import { HeaderComponent } from '@components/header/header.component';
     MatButtonModule,
     HeaderComponent,
     BannerComponent,
-    FieldSearchComponent
+    FieldSearchComponent,
+    CardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
