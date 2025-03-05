31a9e605fef560d9b2f93552866b98fac41c85a8
Alif Razan Saputra <alifrazanit@gmail.com>
2025-03-06 02:12:30 +0700

    perbaikan tampilan

--- src/app/pages/pokemon/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pages/pokemon/pokemon-detail/pokemon-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '@components/header/header.component';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.scss',
   standalone: true
