b640e79e7230ee697425a4ff5b32b1a8672c2fd8
Alif Razan Saputra <alifrazanit@gmail.com>
2025-03-06 10:47:03 +0700

    perbaikan

--- src/app/components/field-search/field-search.component.ts
+++ src/app/components/field-search/field-search.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-field-search',
   imports: [
@@ -11,11 +13,14 @@ import {MatIconModule} from '@angular/material/icon';
     MatFormFieldModule,
     MatSelectModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule
   ],
   templateUrl: './field-search.component.html',
   styleUrl: './field-search.component.scss'
 })
 export class FieldSearchComponent {
-
+  filterForm = new FormGroup({
+    pokemonType: new FormControl(''),
+  });
 }
